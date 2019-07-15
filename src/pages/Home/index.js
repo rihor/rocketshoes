f5553f2c62dcdfa6a307ecc2889996c7c9ae7d4c
Pedro Pinho <rihor1000@gmail.com>
2019-07-15 18:03:40 -0300

    Utilizando o redux Contando quantidade de itens no carrinho

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { connect } from 'react-redux';
 
 import { ProductList } from './styles';
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   };
@@ -21,6 +22,12 @@ export default class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+    // dispara uma ACTION para o redux
+    dispatch({ type: 'ADD_TO_CART', product });
+  };
+
   render() {
     const { products } = this.state;
     return (
@@ -31,7 +38,10 @@ export default class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" /> 3
               </div>
@@ -44,3 +54,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);
