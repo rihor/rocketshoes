8ea7860477efb1734257fcc04a333c4ae259a804
Pedro Pinho <rihor1000@gmail.com>
2019-07-14 21:12:16 -0300

    Configurando rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
