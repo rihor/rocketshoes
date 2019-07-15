f2c3afd6aafb7b6dc0543f6db4eabc58fe59bbc8
Pedro Pinho <rihor1000@gmail.com>
2019-07-15 19:15:27 -0300

    Checando no reducer se o produto é repetido

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,7 +1,27 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.product];
+      return produce(state, draft => {
+        // checa se o produto é repetido
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({ ...action.product, amount: 1 });
+        }
+      });
+    /**
+     * vou deixar isso aqui porque eu consegui fazer sem usar a lib
+     * const productIndex = state.findIndex(p => p.id === action.product.id);
+     * if (productIndex >= 0) {
+     *  state[productIndex].amount += 1;
+     *  return state;
+     * }
+     * return [...state, { ...action.product, amount: 1 }];
+     */
+
     default:
       return state;
   }
