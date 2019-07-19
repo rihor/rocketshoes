9dbd836a95bdaf2cf3e104f3dad5c64e56a6e7a4
Pedro Pinho <rihor1000@gmail.com>
2019-07-19 12:12:22 -0300

    Adicionado regras para hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,5 +28,7 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'no-param-reassign': 'off',
     'no-console': ['error', { allow: ['tron'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
 };
