6b7a4ea2bef892c578c593905f5a4042da410578
Pedro Pinho <rihor1000@gmail.com>
2019-07-19 12:18:55 -0300

    Adicionando react-hooks como plugin no ESLint Tinha esquecido😔

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
