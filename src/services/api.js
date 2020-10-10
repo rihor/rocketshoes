4c8a6955fcaf8dc85e7e04632184afb483d5b5eb
Pedro Pinho <rihor1000@gmail.com>
2020-10-10 09:27:55 -0300

    Removendo server.json e usando json_server online

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://my-json-server.typicode.com/rihor/rocketshoes_json_server',
 });
 
 export default api;
