b2e6f4fc304bf7e22bece89d789c3197339730d2
Pedro Pinho <rihor1000@gmail.com>
2020-10-10 11:07:34 -0300

    Making Cart page responsive

--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -27,20 +27,99 @@ export const Container = styled.div`
       }
     }
   }
+
+  @media only screen and (max-width: 650px) {
+    footer {
+      flex-direction: column-reverse;
+
+      button {
+        width: 100%;
+        margin-top: 10px;
+      }
+    }
+  }
 `;
 
 export const ProductTable = styled.table`
   width: 100%;
+  border-collapse: collapse;
+
+  thead {
+    @media (max-width: 650px) {
+      display: none;
+    }
+  }
 
   thead th {
     color: #999;
     text-align: left;
     padding: 12px;
+
+    &:nth-child(3) {
+      text-align: center;
+    }
+  }
+
+  tbody tr {
+    border-bottom: 1px solid #eee;
+
+    @media (max-width: 650px) {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+    }
   }
 
   tbody td {
     padding: 12px;
-    border-bottom: 1px solid #eee;
+
+    &:first-child {
+      @media (max-width: 650px) {
+        padding-left: 0;
+        padding-bottom: 0;
+      }
+    }
+
+    &:nth-child(2) {
+      @media (max-width: 650px) {
+        flex: 1 1 60%;
+      }
+      span {
+        @media (max-width: 650px) {
+          font-size: 14px;
+          color: #666;
+        }
+      }
+    }
+
+    &:nth-child(3),
+    &:nth-child(4),
+    &:nth-child(5) {
+      @media (max-width: 650px) {
+        padding-top: 0;
+        padding-bottom: 0;
+      }
+    }
+
+    &:nth-child(3) {
+      @media (max-width: 650px) {
+        padding: 0;
+      }
+    }
+
+    &:nth-child(4) {
+      @media (max-width: 650px) {
+        flex: 1;
+        text-align: center;
+        font-size: 16px;
+      }
+    }
+
+    &:nth-child(5) {
+      @media (max-width: 650px) {
+        padding: 12px 0;
+      }
+    }
   }
 
   img {
@@ -48,13 +127,12 @@ export const ProductTable = styled.table`
   }
 
   strong {
-    color: #333;
     display: block;
   }
 
   span {
     display: block;
-    margin-top: 5px;
+    margin-top: 4px;
     font-size: 18px;
     font-weight: bold;
   }
@@ -62,20 +140,31 @@ export const ProductTable = styled.table`
   div {
     display: flex;
     align-items: center;
+    justify-content: center;
 
     input {
       border: 1px solid #ddd;
-      border-radius: 4px;
+      border-radius: 3px;
       color: #666;
       padding: 6px;
       width: 50px;
+      text-align: center;
+      @media (max-width: 650px) {
+        width: 36px;
+      }
     }
   }
 
   button {
     background: none;
     border: 0;
-    padding: 6px;
+    padding: 6px 8px;
+    svg {
+      @media (max-width: 650px) {
+        width: 24px;
+        height: 24px;
+      }
+    }
   }
 `;
 
